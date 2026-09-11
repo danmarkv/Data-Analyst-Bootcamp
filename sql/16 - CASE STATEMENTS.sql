5444c6046e02bdbb58b0877c3b2f15369f48965b
danmarkv <dan.mark.villaplana@gmail.com>
2026-09-11 20:49:50 +0800

    Add SQL file for CASE STATEMENTS

---
+++ sql/16 - CASE STATEMENTS.sql
@@ -0,0 +1,21 @@
+# CASE STATEMENTS
+
+SELECT first_name, last_name, age,
+CASE
+	WHEN age <= 30 THEN 'Young'
+    WHEN age BETWEEN 31 and 50 THEN 'Old'
+    WHEN age > 50 THEN "On Death's Door"
+END age_bracket
+FROM employee_demographics;
+
+SELECT first_name, last_name, salary,
+CASE
+	WHEN salary < 50000 THEN salary + (salary * 0.05)
+    WHEN salary >= 50000 THEN salary + (salary * 0.07)
+END new_salary,
+CASE
+	WHEN dept_id = 6 THEN salary * 0.1
+END bonus
+FROM employee_salary;
+
+# SUBQUERIES
